5955de7a666687ecfc4530709350fadf2585865e
Raunak Agarwal <agarwalraunak2000@gmail.com>
2023-09-04 02:48:03 +0530

    upgraded to lev 4 security

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
+
 const express= require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");// for encryption
+// const encrypt=require("mongoose-encryption");// for encryption lev2
+// var md5 = require('md5');//lev3
+//better than md5 only 17000 hashing per sec
+const bcrypt = require('bcrypt');//lev4
+
+const saltRounds = 10;
+
+
+
+
+
 const app=express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -13,8 +25,7 @@ const userSchema=new mongoose.Schema ({
     email:String,
     password:String 
 });
-var secret = "this is our little secret";
-userSchema.plugin(encrypt, { secret: secret,encryptedFields:["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields:["password"] });
 //only password encrypt hoga
 //apne app chaleg save aur find mai
 
@@ -32,25 +43,39 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser=new User({
-        email:req.body.username,
-        password:req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    
+        const newUser=new User({
+            email:req.body.username,
+            password:hash
+        });
+        newUser.save();
+        res.render("secrets");
+
     });
-    newUser.save();
-    res.render("secrets");
+
+
 }); 
 
 app.post("/login",function(req,res){
 const username=req.body.username;
+// const password=md5(req.body.password);
 const password=req.body.password;
-
 User.findOne({email:username}).then((foundUser)=>{
-if(foundUser.password===password){
+// if(foundUser.password===password){
+//     res.render("secrets");
+// }
+
+bcrypt.compare(password, foundUser.password, function(err, result) {
+    if(result===true)
     res.render("secrets");
-}
-else{
+    else
     console.log('wrong password');
-}
+});
+
+
 
 });
 
